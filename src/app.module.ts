fc4cf740186ca0d129b56fbe1763ff44d12d5ffb
mnseok <min.seok@g.skku.edu>
2024-01-08 10:37:54 +0900

    feat(be): add file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { UploadsModule } from './uploads/uploads.module';
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    PrismaModule,
+    MulterModule.register({
+      dest: './uploads',
+    }),
+    UploadsModule,
+  ],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHook(app);
+
   await app.listen(3000);
 }
 bootstrap();
